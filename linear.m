a475c0e430f78c059b4a4dbbebff3c42865284ff
Archie <archie@Archibalds-Air.home>
2020-01-18 23:37:33 +0000

    confused, trying to work out backprop implementation

--- linear.m
+++ linear.m
@@ -1,26 +1,24 @@
 classdef linear
     properties
         coefficients;
-        bias;
         learning_rate;
         dim;
     end
     methods
         function obj = linear(dim_in, learning_rate_in)
             obj.dim = dim_in;
-            obj.coefficients = rand(dim_in, 1);
+            obj.coefficients = rand(dim_in + 1, 1);
             obj.learning_rate = learning_rate_in;
-	    obj.bias = rand();
         end
         function y = forward(obj, x)
-            y = x * obj.coefficients + obj.bias;
+            y = [x,1] * obj.coefficients;
         end
-        function cost_function = backward(obj, expectation)
-            symbols = sym('x',[1 obj.dim]);
-            cost_function = (expectation - obj.forward(symbols))^2;
+        function optimiser = backward(obj, input, expectation)
+            symbols = sym('a',[1 obj.dim+1]);
+            cost_function = sqrt((expectation - [input,1]*symbols')^2);
 	    % % working up to here
-	    % optimiser = matlabFunction(gradient(cost_function));
-	    % obj.coefficients = obj.coefficients - optimiser(x);
+	    optimiser = matlabFunction(gradient(cost_function));
+	    %obj.coefficients = obj.coefficients - optimiser(input);
         end
     end
 end
