1e1bf07c26b31a415ea9fc27f996fb8e3ffdd119
Archie <archie@dyn3214-123.wlan.ic.ac.uk>
2020-01-21 15:08:01 +0000

    pre much about

--- main.m
+++ main.m
@@ -6,14 +6,17 @@ for i = 1:10
 	x = randi(200);
 	data_set = [data_set; [x, x]];
 end
-
+figure();
 plot(data_set(1:end, 1), data_set(1:end, 2), '*')
+figure();
 for i = 1:100
 	for data = data_set
 		opt = a.backward(data(1),data(2));
-		%a.coefficients
+		a.coefficients
 		tmp = normalize(opt(a.coefficients(1), a.coefficients(2)));
 		a.coefficients = a.coefficients - a.learning_rate*tmp;
-	end 
+    end
+    plot(i, a.coefficients(1), '*r')
+    hold on
 end
-
+hold off
